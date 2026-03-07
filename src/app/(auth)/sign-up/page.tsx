300fab654bcc4f20c92acd1b38935154ac7daeb3
Madhavan-M07 <madhavan09072003@gmail.com>
2026-03-08 02:42:20 +0530

    feat:signin page done

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+
+const Page = () => {
+  console.log("signup page");
+  return <SignUpView />;
+};
+
+export default Page;
---
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Card } from "@/components/ui/card";
+
+export const SignUpView = () => {
+    console.log("signup view");
+    return (
+        <Card>
+            Sign up view
+        </Card>
+    );
+};
